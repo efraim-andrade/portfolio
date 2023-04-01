0586c9fb0972c3e2363e5f110129aca66f5081df
Efraim Andrade <efraim_andrade@Efraims-MacBook-Air.local>
2023-04-01 17:40:32 -0300

    feat: 🎸 GoogleGa
    added google ga

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import { setConfig } from 'react-hot-loader';
 
+import ReactGA from 'react-ga4';
+
 import App from './App';
 
 // setConfig({
 //   reloadHooks: false,
 // });
+
+ReactGA.initialize('UA-138749706-1');
 
 ReactDOM.render(<App />, document.getElementById('root'));
